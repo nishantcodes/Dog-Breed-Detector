41df52c07f7fc79b82e421b273e5d212771ce803
Nishant Dhingra <nishantdhingrabtlknps@gmail.com>
2022-01-13 02:48:44 +0530

    Update main.py
    Adjusted responsiveness

--- main.py
+++ main.py
@@ -187,7 +187,9 @@ async def main():
 
 head_html = """
 <head>
-    <met name = "viewport" content="width=device-width, initial-scale=1"/>\
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name = "viewport" content="width=device-width, initial-scale=1"/>
     <title>🐶 Dog Breed Guesser</title>   
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> 
